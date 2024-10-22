22af873c84eda40123485699063f592f7eed969a
Sergio Barreras <sergiobg.isc@gmail.com>
2024-10-21 22:56:26 -0700

    feat: slots con nombre

--- src/modules/projects/views/ProjectsView.vue
+++ src/modules/projects/views/ProjectsView.vue
@@ -29,18 +29,40 @@
     @value="onNewValue"
   />
 
+  <custom-modal :open="customModalOpen">
+    <template #header>
+      <h1 class="text-3xl">Titulo</h1>
+    </template>
+    <template #body>
+      <p>Ut aliqua nulla ad cupidatat irure excepteur Lorem.</p>
+    </template>
+    <template #footer-actions>
+      <div class="flex justify-end">
+        <button @click="customModalOpen = false" class="btn mr-4">Close</button>
+        <button @click="customModalOpen = false" class="btn btn-primary">Aceptar</button>
+      </div>
+    </template>
+  </custom-modal>
+
   <fab-button @click="modalOpen = true">
     <add-circle />
+  </fab-button>
+
+  <fab-button @click="customModalOpen = true" position="bottom-left">
+    <edit-icon />
   </fab-button>
 </template>
 
 <script lang="ts" setup>
+import CustomModal from '@/modules/common/components/CustomModal.vue';
 import FabButton from '@/modules/common/components/FabButton.vue';
 import InputModal from '@/modules/common/components/InputModal.vue';
 import AddCircle from '@/modules/common/icons/AddCircle.vue';
+import EditIcon from '@/modules/common/icons/EditIcon.vue';
 import { ref } from 'vue';
 
 const modalOpen = ref(false);
+const customModalOpen = ref(false);
 
 const onNewValue = (projectName: string) => {
   console.log({ projectName });
