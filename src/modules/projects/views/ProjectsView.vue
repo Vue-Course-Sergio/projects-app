5aca28ccbfac8c65844f1ffe524ce98dc870e9d4
Sergio Barreras <sergiobg.isc@gmail.com>
2024-10-21 22:05:52 -0700

    feat: modal con slot

--- src/modules/projects/views/ProjectsView.vue
+++ src/modules/projects/views/ProjectsView.vue
@@ -20,12 +20,29 @@
     </table>
   </div>
 
-  <fab-button @click="() => console.log('Hola')">
+  <input-modal
+    placeholder="Ingrese el nombre del proyecto"
+    title="Nuevo proyecto"
+    subtitle="Dale un nombre único a tu proyecto"
+    :open="modalOpen"
+    @close="modalOpen = false"
+    @value="onNewValue"
+  />
+
+  <fab-button @click="modalOpen = true">
     <add-circle />
   </fab-button>
 </template>
 
 <script lang="ts" setup>
 import FabButton from '@/modules/common/components/FabButton.vue';
+import InputModal from '@/modules/common/components/InputModal.vue';
 import AddCircle from '@/modules/common/icons/AddCircle.vue';
+import { ref } from 'vue';
+
+const modalOpen = ref(false);
+
+const onNewValue = (projectName: string) => {
+  console.log({ projectName });
+};
 </script>
