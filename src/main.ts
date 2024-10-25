de8cbb749069730b17ffc3761301954e96823390
Sergio Barreras <sergiobg.isc@gmail.com>
2024-10-24 23:42:19 -0700

    test: main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
 
-const app = createApp(App);
 const pinia = createPinia();
+const app = createApp(App);
 
-app.use(router);
 app.use(pinia);
+app.use(router);
 
 app.mount('#app');
