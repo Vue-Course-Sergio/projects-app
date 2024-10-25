32ac109f72b19d1e39d5d66777e280a2285f0977
Sergio Barreras <sergiobg.isc@gmail.com>
2024-10-24 18:18:42 -0700

    style: vista de proyectos

--- src/modules/projects/views/ProjectView.vue
+++ src/modules/projects/views/ProjectView.vue
@@ -1,16 +1,57 @@
 <template>
-  <bread-crumbs :name="project?.name ?? 'No name'" />
+  <div class="w-full">
+    <section class="m-2">
+      <bread-crumbs :name="project?.name ?? 'No name'" />
+    </section>
+
+    <section class="m-2">
+      <div class="overflow-x-auto">
+        <table class="table">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th class="w-14">Completada</th>
+              <th>Tarea</th>
+              <th>Completada en</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="hover">
+              <th>2</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+            </tr>
+            <tr class="hover">
+              <th></th>
+              <td>
+                <input
+                  type="text"
+                  class="input input-primary w-full opacity-60 transition-all hover:opacity-100 focus:opacity-100"
+                  placeholder="Nueva tarea"
+                />
+              </td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import BreadCrumbs from '@/modules/common/components/BreadCrumbs.vue';
-import { useProjectsStore } from '../store/projects.store';
+
 import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
 import type { Project } from '../interfaces/project.interface';
+import { useProjectsStore } from '../store/projects.store';
 
 interface Props {
   id: string;
 }
+
+const router = useRouter();
 
 const props = defineProps<Props>();
 const projectStore = useProjectsStore();
@@ -21,6 +62,7 @@ watch(
   () => props.id,
   () => {
     project.value = projectStore.projectList.find((p) => p.id === props.id);
+    if (!project.value) router.replace('/');
   },
   {
     immediate: true,
